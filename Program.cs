f405805b7104a4d5cad30097ea925ea55a32b48c
tanuj1080 <tanujajitpratapsingh@usf.edu>
2024-02-28 11:24:04 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -1,24 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-//5,72,3,14,7,1
+//Name: Tanuj Ajitpratap Singh
+//Date: 28th February 2024
+//This code implements bubble sort algorithm on the input array.
 
 
+//input array
 int[] arr = { 5, 72, 3, 14, 7, 1};
+
+//function call to implement bubbleSort
 bubbleSort(arr);
+
+//Print the sorted array
 Console.WriteLine("After bubble sort");
 for (int i = 0; i < arr.Length; i++)
 {
     Console.Write("  " + arr[i]);
 }
+//bubblesort function declaration with implementation
 static void bubbleSort(int[] arr)
 {
+    //outer loop for iterating over the input array
     for(int i = 0; i < arr.Length; i++)
     {
+        //inner loop to work on sorting with respect to each element
         for(int j = 0; j < arr.Length - i - 1; j++)
         {
+            //comparing adjacent values
             if (arr[j+1] < arr[j])
             {
+                //swapping values
                 int temp = arr[j+1];
                 arr[j+1] = arr[j]; 
                 arr[j] = temp;
